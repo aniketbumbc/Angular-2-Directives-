4df1574b1b9a198eabfe8a8e0e70fdf8f9904ee6
aniket.umbc <aniket.umbc@gmail.com>
2017-03-07 18:25:28 -0500

    structual Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+private switch=true;
+private names=["apple","Mango","Banana","Pine"];
+onclick(){
+  this.switch=!this.switch;
 
+}
 }
